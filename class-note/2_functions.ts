65451ae67a10811121ca0df8e8606c3ccf4360ad
dudcheol <95_pyc@naver.com>
2021-09-13 16:54:12 +0900

    함수 타입 - 옵셔널 파라미

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -14,3 +14,11 @@ function sum2(a: number, b: number): number {
     return a + b;
 }
 sum2(10, 20, 30, 40); // 2개의 인수가 필요한데 4개를 가져오고 있음
+
+// 함수의 옵셔널 파라미터(?)
+// 필요에 따라 인자를 넣어도 되고 안넣어도 되게끔 할 수 있음
+function log(a: string, b?: string) {
+
+}
+log('hello world');
+log('hello ts', 'abc');
