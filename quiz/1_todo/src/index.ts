cfb2e55af4b24f26337c3984dc943c32341afd25
dudcheol <95_pyc@naver.com>
2021-09-13 17:57:45 +0900

    실습 내 풀이

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,8 +1,8 @@
-let todoItems;
+let todoItems: Array<{id:number, title:string, done:boolean}>;
 
 // api
-function fetchTodoItems() {
-  const todos = [
+function fetchTodoItems(): Array<{id:number, title:string, done:boolean}> {
+  const todos: Array<{id:number, title:string, done:boolean}> = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
     { id: 3, title: '스크립트', done: false },
@@ -16,35 +16,38 @@ function fetchTodos() {
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: {id:number, title:string, done:boolean}): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: {id:number, title:string, done:boolean}): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): {id:number, title:string, done:boolean} {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): Array<{id:number, title:string, done:boolean}> {
   return todoItems.filter(item => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  let id: number = todoItems[todoItems.length - 1].id;
+  addTodo({id:id+1, title:'새 할 일', done:false});
+  addTodo({id:id+2, title:'새 할 일', done:false});
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
