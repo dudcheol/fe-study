3598e371e18240279ab5f4a7dcf14128426f1f18
dudcheol <95_pyc@naver.com>
2021-09-16 15:53:47 +0900

    제네릭 살펴보기

---
+++ class-note/8_generics.ts
@@ -0,0 +1,46 @@
+// function logText(text) {
+//     console.log(text);
+//     return text;
+// }
+// logText(10); // 숫자 10
+// logText('하이'); // 문자열 하이
+// logText(true); // 진위값 true
+
+// function logText<T>(text: T): T {
+//     console.log(text);
+//     return text;
+// }
+// logText<string>('하이'); // 호출하는 시점에 타입을 넘겨줄 수 있다
+
+// function logText(text: string) {
+//     console.log(text);
+//     // text.split('').reverse().join('');
+//     return text;
+// }
+
+// function logNumber(num: number) {
+//     console.log(num);
+//     return num;
+// }
+
+// function logText(text: string | number) {
+//     console.log(text);
+//     return text;
+// }
+
+// const a = logText('a');
+// a.split(''); // number일 수 있어서 split을 사용할 수 없음
+// logText(10);
+// const num = logNumber(10);
+// logText(true);
+
+function logText<T>(text: T): T {
+    console.log(text);
+    return text;
+}
+const str = logText<string>('abc');
+str.split('');
+const login = logText<boolean>(true);
+
+// logText('a');
+// logText(10);
