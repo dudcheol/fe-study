13d34329c39fd1ba6ec405a7c60c9da6e69d4470
dudcheol <95_pyc@naver.com>
2021-09-16 16:14:52 +0900

    제네릭 타입 제한

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -57,3 +57,39 @@ interface Dropdown<T> {
     selected: boolean;
 }
 const obj: Dropdown<string> = { value: 'abc', selected: false };
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//     console.log(text.length);
+//     text.forEach(function (text) {
+//         console.log(text);
+//     })
+//     return text;
+// }
+// logTextLength<string>(['hi']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T{
+    text.length;
+    return text;
+}
+logTextLength('a'); // string은 length를 제공하므로 가능
+logTextLength(10); // number는 length를 제공하지 않으므로 오류!
+logTextLength({ length: 10 }); // 해당 객체는 length가 정의되어 있기 때문에 가능
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem{
+    name: string;
+    price: number;
+    stock: number;
+}
+// ShoppingItem에 있는 key들 중 한가지가 제네릭이 됨
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption;
+}
+// getShoppingItemOption(10);
+// getShoppingItemOption('a');
+getShoppingItemOption("name");
