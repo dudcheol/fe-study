425ffba12107beb4ebf3aed01873fa51a829ccc2
dudcheol <95_pyc@naver.com>
2022-01-15 12:57:26 +0900

    loadable 추가

--- react/suspense/src/App.js
+++ react/suspense/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Suspense, useState } from 'react';
+import loadable from '@loadable/component';
 
 // React.lazy - 컴포넌트를 렌더링하는 시점에서 비동기적으로 로딩할 수 있게 해주는 유틸 함수
-const SplitMe = React.lazy(()=>import('./SplitMe'));
+// loadable - 코드 스플리팅을 편하게 하도록 도와주는 서드파티 라이브러리. 서버 사이드 렌더링을 지원함
+const SplitMe = loadable(()=>import('./SplitMe'));
 
 function App() {
   const [visible, setVisible] = useState(false);
