c818e21a49e3bec776b72a67421bf9ba07bb84a4
dudcheol <95_pyc@naver.com>
2022-01-15 12:59:56 +0900

    preload

--- react/suspense/src/App.js
+++ react/suspense/src/App.js
@@ -5,18 +5,23 @@ import loadable from '@loadable/component';
 
 // React.lazy - 컴포넌트를 렌더링하는 시점에서 비동기적으로 로딩할 수 있게 해주는 유틸 함수
 // loadable - 코드 스플리팅을 편하게 하도록 도와주는 서드파티 라이브러리. 서버 사이드 렌더링을 지원함
-const SplitMe = loadable(()=>import('./SplitMe'));
+const SplitMe = loadable(()=>import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
-  }
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
         <Suspense fallback={<div>loading...</div>}>
           {visible && <SplitMe />}
         </Suspense>
