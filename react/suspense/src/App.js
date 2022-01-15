18a6ca4c6d4a98317dd87348d6b7a908dce2b1a0
dudcheol <95_pyc@naver.com>
2022-01-15 12:53:57 +0900

    suspense, react.lazy 이용

--- react/suspense/src/App.js
+++ react/suspense/src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Suspense, useState } from 'react';
+
+// React.lazy - 컴포넌트를 렌더링하는 시점에서 비동기적으로 로딩할 수 있게 해주는 유틸 함수
+const SplitMe = React.lazy(()=>import('./SplitMe'));
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
       </header>
     </div>
   );
